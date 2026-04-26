0445db1efb7db98dc6ca1c92cc600f297a6d7f9e
Ben Waters <thebenswaters@gmail.com>
2026-04-25 20:46:37 -0700

    attempt to fix logger

--- sharksice/__main__.py
+++ sharksice/__main__.py
@@ -1,14 +1,31 @@
 import asyncio
+import os
+import logging
 
 from .calendars import generate_and_save_calendars
 from .events import fetch_and_store_events
 
 async def main():
     """Main entry point."""
+    logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('sync.log', mode='a'),
+        logging.StreamHandler()
+    ]
+)
+    logger = logging.getLogger(__name__)
+    logger.info("Start fetch_and_store_events")
     success = await fetch_and_store_events()
-    success = await generate_and_save_calendars()
+    logger.info(f"result of fetch_and_store_events {success}")
     if not success:
-        exit(1)
+        os.exit(1)
+    logger.info("Start generate_and_save_calendars")
+    success = await generate_and_save_calendars()
+    logger.info(f"result of generate_and_save_calendars {success}")
+    if not success:
+        os.exit(1)
 
 
 if __name__ == "__main__":
